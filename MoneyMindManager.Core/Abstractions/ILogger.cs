deabbe2be9e0a741525b23a881e1ebbcc999baf4
Khaled Abu Al-Majd <khaledabualmajd06@gmail.com>
2026-02-11 17:39:27 +0200

    feat: add ILogger Interface (MoneyMindManager.Core) and EventLogLogger Implementation (Infrastracture). add LoggingSourceName and LoggingLogName To App.Config #1

---
+++ MoneyMindManager.Core/Abstractions/ILogger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyMindManager.Core
+{
+    public interface ILogger
+    {
+        bool Log(string message);
+
+        bool LogError(string message,Exception ex);
+
+        bool LogSuccess(string message);
+
+        bool LogWarning(string message);
+
+        bool LogInfo(string message);
+    }
+}
---
+++ MoneyMindManager.Infrastructure/Logging/EventLogLogger.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MoneyMindManager.Core;
+
+namespace MoneyMindManager.Infrastructure.Logging
+{
+    public class EventLogLogger : ILogger
+    {
+        private readonly string _sourceName;
+
+        public EventLogLogger(string sourceName)
+        {
+            _sourceName = sourceName;
+        }
+
+        private bool _Log(string message, EventLogEntryType eventLogEntryType)
+        {
+            try
+            {
+                EventLog.WriteEntry(_sourceName, message,eventLogEntryType);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        public bool Log(string message)
+        {
+            try
+            {
+                EventLog.WriteEntry(_sourceName, message);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool LogError(string message, Exception ex)
+        {
+            string errorMessage = $"An error happped with message: {message}, Exception: {ex.Message}";
+            return _Log(errorMessage, EventLogEntryType.Error);
+        }
+
+        public bool LogSuccess(string message)
+        {
+            return _Log(message, EventLogEntryType.SuccessAudit);
+        }
+
+        public bool LogWarning(string message)
+        {
+            return _Log(message, EventLogEntryType.Warning);
+        }
+
+        public bool LogInfo(string message)
+        {
+            return _Log(message, EventLogEntryType.Information);
+        }
+    }
+}
