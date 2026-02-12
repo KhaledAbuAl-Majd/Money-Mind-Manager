4f4eda457eb354630db81a9ffc589ab156415437
Khaled Abu Al-Majd <khaledabualmajd06@gmail.com>
2026-02-12 21:22:14 +0200

    feat: create IResultFactory"to return IResultFactory dynamic"and ResultFactory. create currencyService,ICurrencyService,SQLCurrencyRepository. create IDatabaseSettings,SQLDataBaseSettings #1

---
+++ MoneyMindManager.Application/Services/Currency/CurrencyService.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MoneyMindManager.Application.Abstractions.Handlers;
+using MoneyMindManager.Application.Abstractions.Services;
+using MoneyMindManager.Core.Abstractions;
+using MoneyMindManager.Domain.Abstractions;
+using MoneyMindManager.Domain.Entities;
+using MoneyMindManager.Shared.DTOs.Currency;
+
+namespace MoneyMindManager.Application.Services.Currency
+{
+    public class CurrencyService : ICurrencyService
+    {
+        private readonly ICurrencyRepository _currencyRepository;
+        private readonly IResultFactory _resultFactory;
+        private CurrencyDTO _getCurrencyDTOFromCurrency(MoneyMindManager.Domain.Entities.Currency currency)
+        {
+            return new CurrencyDTO(currency.CurrencyID, currency.CurrencyName, currency.CurrencySymbol);
+        }
+        private MoneyMindManager.Domain.Entities.Currency _getCurrencyFromCurrencyDTO(CurrencyDTO currencyDTO)
+        {
+            return new MoneyMindManager.Domain.Entities.Currency(currencyDTO.CurrencyID, currencyDTO.CurrencyName, currencyDTO.CurrencySymbol);
+        }
+        public CurrencyService(ICurrencyRepository currencyRepository, IResultFactory resultFactory)
+        {
+            this._currencyRepository = currencyRepository;
+            this._resultFactory = resultFactory;
+        }
+        public async Task<IResult<CurrencyDTO>> GetByID(byte currencyID)
+        {
+            var handler = _resultFactory.Create<CurrencyDTO>();
+
+            var result = await _currencyRepository.GetByID(currencyID);
+            if (result is null)
+                return handler.Failure($"an error occured at get currency id");
+
+            if (!result.IsSuccess)
+                return handler.Failure(result?.ErrorMessage);
+
+            if (result.Data is null)
+                return handler.Failure("The Currency Data is null");
+
+            return handler.Success(_getCurrencyDTOFromCurrency(result.Data));
+        }
+
+        public async Task<IResult<CurrencyDTO>> GetByName(string currencyName)
+        {
+            var handler = _resultFactory.Create<CurrencyDTO>();
+
+            var result = await _currencyRepository.GetByName(currencyName);
+            if (result is null)
+                return handler.Failure($"an error occured at get currency name");
+
+            if (!result.IsSuccess)
+                return handler.Failure(result?.ErrorMessage);
+
+            if (result.Data is null)
+                return handler.Failure("The Currency Data is null");
+
+            return handler.Success(_getCurrencyDTOFromCurrency(result.Data));
+        }
+
+        public async Task<IResult<IEnumerable<CurrencyDTO>>> GetAll()
+        {
+            var handler = _resultFactory.Create<IEnumerable<CurrencyDTO>>();
+            var result = await _currencyRepository.GetAll();
+
+            if (result is null)
+                return handler.Failure("an error occured at get all currencies");
+
+            if (!result.IsSuccess)
+                return handler.Failure(result?.ErrorMessage);
+
+            if (result.Data is null)
+                return handler.Failure("The Data Is null at get all currencies");
+
+            return handler.Success(result.Data.Select(entity => _getCurrencyDTOFromCurrency(entity)));
+        }
+    }
+}
---
+++ MoneyMindManager.Infrastructure/Repositories/SQLServer/SQLDatabaseSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MoneyMindManager.Domain.Abstractions;
+using System.Configuration;
+
+
+namespace MoneyMindManager.Infrastructure.Repositories.SQLServer
+{
+    public class SQLDatabaseSettings:IDatabaseSettings
+    {
+        public string ConnectionString { get; } = ConfigurationManager.ConnectionStrings["MoneyMindManagerConnectionString"].ConnectionString;
+    }
+}
