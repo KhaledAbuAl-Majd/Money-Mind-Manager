8766f2baf508f92c65b98aa2cb3f23f5584ab50d
Khaled Abu Al-Majd <khaledabualmajd06@gmail.com>
2026-02-18 20:30:40 +0200

    refactoring(Reports-General): create (MainKpisReportModel and MonthlyFlowReportModel) at core layer and creating IGeneralReportRepository (domain layer) #1. move UserSummary and PersmissionInfo to Models Folder.

---
+++ MoneyMindManager.Core/Models/Reports/MonthlyFlowReportModel.cs
@@ -0,0 +1,28 @@
+﻿namespace MoneyMindManager.Core.Models.Reports
+{
+    public class MonthlyFlowReportModel
+    {
+        public byte mon { get; set; }
+        public short Year { get; set; }
+        public decimal Income { get; set; }
+        public decimal NetExpense { get; set; }
+        public decimal NetCashFlow { get; set; }
+        public decimal TotalIncome { get; set; }
+        public decimal TotalNetExpense { get; set; }
+        public decimal TotalNetCashFlow { get; set; }
+
+
+        public MonthlyFlowReportModel(byte month, short year, decimal income, decimal netExpense, decimal netCashFlow,
+            decimal totalIncome, decimal totalNetExpense, decimal totalNetCashFlow)
+        {
+            this.mon = month;
+            this.Year = year;
+            this.Income = income;
+            this.NetExpense = netExpense;
+            this.NetCashFlow = netCashFlow;
+            this.TotalIncome = totalIncome;
+            this.TotalNetExpense = totalNetExpense;
+            this.TotalNetCashFlow = totalNetCashFlow;
+        }
+    }
+}
---
+++ MoneyMindManager.Domain/Abstractions/Repositories/Reports/IGeneralReportRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MoneyMindManager.Core.Abstractions;
+using MoneyMindManager.Core.Models.Reports;
+
+namespace MoneyMindManager.Domain.Abstractions.Repositories.Reports
+{
+    public interface IGeneralReportRepository
+    {
+        IResult<Task<List<MonthlyFlowReportModel>>> GetMonthlyFlow(DateTime startDate, DateTime EndDate, short accountID);
+        IResult<Task<MainKpisReportModel>> GetMainKPIS(short accountID);
+    }
+}
