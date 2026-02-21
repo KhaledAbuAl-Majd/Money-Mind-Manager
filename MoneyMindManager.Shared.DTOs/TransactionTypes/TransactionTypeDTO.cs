c63caf261bf70397e60af1804bbc4c0490edb923
Khaled Abu Al-Majd <khaledabualmajd06@gmail.com>
2026-02-21 08:32:49 +0200

    feat(TransactionType-Shared.DTOs): Create TransactionTypeDTO #1

---
+++ MoneyMindManager.Shared.DTOs/TransactionTypes/TransactionTypeDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace MoneyMindManager.Shared.DTOs.TransactionTypes
+{
+    public class TransactionTypeDTO
+    {
+        public byte TransactionTypeID { get; }
+        public string TransactionTypeName { get; }
+
+        public TransactionTypeDTO(byte transactionTypeID, string transactionTypeName)
+        {
+            this.TransactionTypeID = transactionTypeID;
+            this.TransactionTypeName = transactionTypeName;
+        }
+    }
+}
