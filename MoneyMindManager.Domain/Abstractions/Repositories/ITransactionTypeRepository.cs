1dee4f7d3e819cc45512f3019bc29c164d39776f
Khaled Abu Al-Majd <khaledabualmajd06@gmail.com>
2026-02-21 08:34:14 +0200

    feat(TransactionType-Domain): Create TransactionType Entity and ITransactionTypeRepository #1

---
+++ MoneyMindManager.Domain/Abstractions/Repositories/ITransactionTypeRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using MoneyMindManager.Core.Abstractions;
+using MoneyMindManager.Domain.Entities;
+
+namespace MoneyMindManager.Domain.Abstractions.Repositories
+{
+    public interface ITransactionTypeRepository
+    {
+        Task<IResult<TransactionType>> GetByID(byte transactionTypeID);
+        Task<IResult<TransactionType>> GetByName(string transactionName);
+        Task<IResult<IEnumerable<TransactionType>>> GetAll();
+    }
+}
---
+++ MoneyMindManager.Domain/Entities/TransactionType.cs
@@ -0,0 +1,19 @@
+﻿namespace MoneyMindManager.Domain.Entities
+{
+    public class TransactionType
+    {
+        public byte TransactionTypeID { get; set; }
+        public string TransactionTypeName { get; set; }
+
+        public TransactionType(byte transactionTypeID, string transactionTypeName)
+        {
+            this.TransactionTypeID = transactionTypeID;
+            this.TransactionTypeName = transactionTypeName;
+        }
+
+        public TransactionType()
+        {
+
+        }
+    }
+}
