4a6d62eac0bf6da2479b5b0d7e30011d4f878482
Khaled Abu Al-Majd <khaledabualmajd06@gmail.com>
2026-02-21 07:16:31 +0200

    feat(Report-Categories-Domain,Core): Create ICategoriesReportRepository,CategoryMonthlyFlowReportModel and TopCategoriesReportModel #1

---
+++ MoneyMindManager.Core/Models/Reports/Categories/TopCategoriesReportModel.cs
@@ -0,0 +1,17 @@
+﻿namespace MoneyMindManager.Core.Models.Reports.Categories
+{
+    public class TopCategoriesReportModel
+    {
+        public string CategoryName { get; set; }
+        public decimal Value { get; set; }
+        public int Ranking { get; set; }
+        public decimal Percentage { get; set; }
+        public TopCategoriesReportModel(string categoryName, decimal value, int ranking, decimal percentage)
+        {
+            this.CategoryName = categoryName;
+            this.Value = value;
+            this.Ranking = ranking;
+            this.Percentage = percentage;
+        }
+    }
+}
