5241d8a1251c7a1bd18d4337c7ada2ea698d74c4
Khaled Abu Al-Majd <khaledabualmajd06@gmail.com>
2026-02-21 07:13:38 +0200

    feat(Report-Debts-Domain,Core): Create IDebtsReportRepository,DebtRepaymentScheduleReportMode, DebtsMonthlyFlowReportModel, TopPeopleDebtsSumRankingReportModel and TopDebtorsRankingReportModel #1

---
+++ MoneyMindManager.Core/Models/Reports/Debts/DebtRepaymentScheduleReportModel.cs
@@ -0,0 +1,21 @@
+﻿namespace MoneyMindManager.Core.Models.Reports.Debts
+{
+    public class DebtRepaymentScheduleReportModel
+    {
+        public byte? mon { get; set; }
+        public short? Year { get; set; }
+        public decimal Receivable { get; set; }
+        public decimal Payables { get; set; }
+        public decimal NetCashFlow { get; set; }
+
+
+        public DebtRepaymentScheduleReportModel(byte? month, short? year, decimal receivable, decimal payables, decimal netCashFlow)
+        {
+            this.mon = month;
+            this.Year = year;
+            this.Receivable = receivable;
+            this.Payables = payables;
+            this.NetCashFlow = netCashFlow;
+        }
+    }
+}
---
+++ MoneyMindManager.Core/Models/Reports/Debts/TopDebtorsRankingReportModel.cs
@@ -0,0 +1,19 @@
+﻿namespace MoneyMindManager.Core.Models.Reports.Debts
+{
+    public class TopDebtorsRankingReportModel
+    {
+        public int PersonID { get; set; }
+        public string PersonName { get; set; }
+        public decimal PersonRemaining { get; set; }
+        public int PersonOrder { get; set; }
+
+
+        public TopDebtorsRankingReportModel(int personID, string personName, decimal personRemaining, int personOrder)
+        {
+            this.PersonID = personID;
+            this.PersonName = personName;
+            this.PersonRemaining = personRemaining;
+            this.PersonOrder = personOrder;
+        }
+    }
+}
