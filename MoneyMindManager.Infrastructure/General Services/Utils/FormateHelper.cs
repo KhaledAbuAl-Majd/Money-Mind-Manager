36c909715fe17867ca5a9e50d8bffaf5d6a72bd6
Khaled Abu Al-Majd <khaledabualmajd06@gmail.com>
2026-02-21 17:04:15 +0200

    feat(FormateHelper-Infrastructure): Create FormateHelper #1

---
+++ MoneyMindManager.Infrastructure/General Services/Utils/FormateHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MoneyMindManager.Core.Abstractions;
+
+namespace MoneyMindManager.Infrastructure.General_Services.Utils
+{
+    public class FormateHelper : IFormateHelper
+    {
+        public DateTime? TryConvertToDateTime(string dateString)
+        {
+            if (!string.IsNullOrWhiteSpace(dateString) && DateTime.TryParse(dateString, out DateTime result))
+                return result;
+
+            return null;
+        }
+    }
+}
