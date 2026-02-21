a850143f6cc6dd81552b06b8e61fce0c39ff2f25
Khaled Abu Al-Majd <khaledabualmajd06@gmail.com>
2026-02-21 08:36:38 +0200

    feat(TransactionType-Application-Mapper): Create ITransactionTypeMapper and TransactionTypeMapper #1

---
+++ MoneyMindManager.Application/Mappers/Mappers Implementaion/TransactionTypeMapper.cs
@@ -0,0 +1,29 @@
+﻿using MoneyMindManager.Application.Mappers.Abstractions;
+using MoneyMindManager.Domain.Entities;
+using MoneyMindManager.Shared.DTOs.TransactionTypes;
+
+namespace MoneyMindManager.Application.Mappers.Mappers_Implementaion
+{
+    public class TransactionTypeMapper : ITransactionTypeMapper
+    {
+        public TransactionType DTOToEntity(TransactionTypeDTO DTO)
+        {
+            if (DTO is null)
+                return null;
+
+            return new TransactionType()
+            {
+                TransactionTypeID = DTO.TransactionTypeID,
+                TransactionTypeName = DTO.TransactionTypeName
+            };
+        }
+
+        public TransactionTypeDTO EntityToDTO(TransactionType entity)
+        {
+            if (entity is null)
+                return null;
+
+            return new TransactionTypeDTO(entity.TransactionTypeID, entity.TransactionTypeName);
+        }
+    }
+}
