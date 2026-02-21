79420cb23b3074c0102fdfa80c57d590ebbff5af
Khaled Abu Al-Majd <khaledabualmajd06@gmail.com>
2026-02-21 17:01:01 +0200

    feat(MainTransaction-Shared.DTOs): Create MainTransactionDTO, MainTransactionFilterDTO and MainTransactionPagedFilterDTO #1

---
+++ MoneyMindManager.Shared.DTOs/MainTransaction/MainTransactionDTO.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace MoneyMindManager.Shared.DTOs.MainTransaction
+{
+    public class MainTransactionDTO
+    {
+        public int? MainTransactionID { get; private set; }
+        public decimal Amount { get; private set; }
+        public DateTime CreatedDate { get; private set; }
+        public short? AccountID { get; private set; }
+        public int? CreatedByUserID { get; private set; }
+        public string CreatedByUserName { get; set; }
+        public byte? TransactionTypeID { get; set; }
+        public string TransactionTypeName { get; set; }
+        public string Purpose { get; private set; }
+        public bool IsLocked { get; private set; }
+        public DateTime TransactionDate { get; private set; }
+
+        public MainTransactionDTO(int? transactionID, decimal amount, DateTime createdDate, short? accountID,
+            int? createdByUserID, byte? tranasactionTypeID, string purpose, bool isLocked, DateTime transactionDate, string transactionTypeName, string createdByUserName)
+        {
+            this.MainTransactionID = transactionID;
+            this.Amount = amount;
+            this.CreatedDate = createdDate;
+            this.AccountID = accountID;
+            this.CreatedByUserID = createdByUserID;
+            this.TransactionTypeID = tranasactionTypeID;
+            this.Purpose = purpose;
+            this.IsLocked = isLocked;
+            this.TransactionDate = transactionDate;
+            this.TransactionTypeName = transactionTypeName;
+            this.CreatedByUserName = createdByUserName;
+        }
+
+        public MainTransactionDTO()
+        {
+            this.MainTransactionID = null;
+            this.Amount = 0;
+            this.CreatedDate = DateTime.MaxValue; ;
+            this.AccountID = null;
+            this.CreatedByUserID = null;
+            this.TransactionTypeID = null;
+            this.Purpose = null;
+            this.TransactionDate = DateTime.MaxValue; ;
+        }
+    }
+}
---
+++ MoneyMindManager.Shared.DTOs/MainTransaction/MainTransactionPagedFilterDTO.cs
@@ -0,0 +1,19 @@
+﻿using MoneyMindManager.Core.Enums;
+
+namespace MoneyMindManager.Shared.DTOs.MainTransaction
+{
+    public class MainTransactionPagedFilterDTO : MainTransactionFilterDTO
+    {
+        public int PageNumber { get; set; }
+
+        public MainTransactionPagedFilterDTO(enTextSearchMode textSearchMode, int pageNumber) : base(textSearchMode)
+        {
+            this.PageNumber = pageNumber;
+        }
+
+        public MainTransactionPagedFilterDTO()
+        {
+            this.PageNumber = 15;
+        }
+    }
+}
