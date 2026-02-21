19c790fdf448f045b1cf1dd3eab4b49306ce3a93
Khaled Abu Al-Majd <khaledabualmajd06@gmail.com>
2026-02-21 17:06:12 +0200

    feat(MainTransaction-Application-Mapper): Create IMainTransactionMapper and MainTransactionMapper #1

---
+++ MoneyMindManager.Application/Mappers/Mappers Implementaion/IMainTransactionMapper.cs
@@ -0,0 +1,13 @@
+﻿using MoneyMindManager.Application.Abstractions.Mappers;
+using MoneyMindManager.Domain.Criteria;
+using MoneyMindManager.Domain.Entities;
+using MoneyMindManager.Shared.DTOs.MainTransaction;
+
+namespace MoneyMindManager.Application.Mappers.Mappers_Implementaion
+{
+    public interface IMainTransactionMapper : IMapper<MainTransaction, MainTransactionDTO>
+    {
+        MainTransactionSearchCriteria MainTransactionFilterDTOToMainTransactionSearchCriteria(MainTransactionFilterDTO filterDTO);
+        MainTransactionPagedSearchCriteria MainTransactionPagedFilterDTOToMainTransactionPagedSearchCriteria(MainTransactionPagedFilterDTO filterDTO);
+    }
+}
