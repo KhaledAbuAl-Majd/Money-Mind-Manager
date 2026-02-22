0a0baedbae7052dbba2a6bdeba72c221c946f532
Khaled Abu Al-Majd <khaledabualmajd06@gmail.com>
2026-02-22 14:55:35 +0200

    feat(FinVoucher-Core): Create FinVoucherExportSummary (voucher list to export), FinVoucherViewSummary (voucher list to display) and FinVoucherEnum #1

---
+++ MoneyMindManager.Core/Models/FinVoucher/FinVoucherViewSummary.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MoneyMindManager.Core.Models.FinVoucher
+{
+    public class FinVoucherViewSummary
+    {
+        public int? VoucherID { get; set; }
+        public string VoucherName { get; set; }
+        public Decimal VoucherValue { get; set; }
+        public int TransactionsCount { get; set; }
+        public DateTime VoucherDate { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string CreatedByUserName { get; set; }
+
+        public FinVoucherViewSummary(int voucherID, string voucherName, int transactionsCount, string createdByUserName,
+            DateTime createdDate, DateTime voucherDate, decimal voucherValue)
+        {
+            this.VoucherID = voucherID;
+            this.VoucherName = voucherName;
+            this.TransactionsCount = transactionsCount;
+            this.CreatedByUserName = createdByUserName;
+            this.CreatedDate = createdDate;
+            this.VoucherDate = voucherDate;
+            this.VoucherValue = voucherValue;
+        }
+    }
+}
