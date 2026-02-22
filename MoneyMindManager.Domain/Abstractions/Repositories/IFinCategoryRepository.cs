09ffafc024e8590d19743e80779ffab667bcf151
Khaled Abu Al-Majd <khaledabualmajd06@gmail.com>
2026-02-22 09:45:01 +0200

    feat(FinCategory-Domain): Create FinCategory Entity, FinCategorySelectPagedSearchCriteria, FinCategoryPagedSearchCriteria, /BudgetCheckCriteria and IFinCategoryRepository #1

---
+++ MoneyMindManager.Domain/Abstractions/Repositories/IFinCategoryRepository.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using MoneyMindManager.Core.Abstractions;
+using MoneyMindManager.Core.Paged_Result_DTOs;
+using MoneyMindManager.Domain.Criteria.IncomeAndExpenseCategory;
+using MoneyMindManager.Domain.Entities;
+
+namespace MoneyMindManager.Domain.Abstractions.Repositories
+{
+    public interface IFinCategoryRepository
+    {
+        Task<IResult<int?>> Add(FinCategory category);
+        Task<IResult<bool>> Update(FinCategory category, int currentUserID);
+        Task<IResult<bool>> Delete(int categoryID, int currentUserID);
+        Task<IResult<FinCategory>> GetByID(int categoryID, int currentUserID);
+        Task<IResult<FinCategory>> GetByName(string categoryName, int currentUserID);
+        Task<IResult<bool>> IsExistByName(string categoryName, int currentUserID);
+        Task<IResult<PagedResultDTO<FinCategory>>> GetAllForSelectOne(FinCategorySelectPagedSearchCriteria criteria, int currentUserID);
+        Task<IResult<PagedResultDTO<FinCategory>>> GetAll(FinCategoryPagedSearchCriteria criteria, int currentUserID);
+        Task<IResult<bool>> IsExceedMonthlyBudget(BudgetCheckCriteria budgetCheckCriteria, int currentUserID);
+    }
+}
---
+++ MoneyMindManager.Domain/Criteria/IncomeAndExpenseCategory/BudgetCheckCriteria.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MoneyMindManager.Domain.Criteria.IncomeAndExpenseCategory
+{
+    public class BudgetCheckCriteria
+    {
+        public int CategoryID { get; set; }
+        public int? TransactionID { get; set; }
+        public decimal? Amount { get; set; }
+        public DateTime TransactionDate { get; set; }
+        public bool? IsReturn { get; set; }
+
+        public BudgetCheckCriteria(int categoryID, int? transactionID, decimal? amount, DateTime transactionDate, bool? isReturn)
+        {
+            this.CategoryID = categoryID;
+            this.TransactionID = transactionID;
+            this.Amount = amount;
+            this.TransactionDate = transactionDate;
+            this.IsReturn = IsReturn;
+        }
+
+        public BudgetCheckCriteria()
+        {
+
+        }
+    }
+}
