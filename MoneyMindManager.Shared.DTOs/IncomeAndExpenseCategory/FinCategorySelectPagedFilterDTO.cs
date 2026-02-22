82867c06a6058d1ac1af32139530a011f37ccbce
Khaled Abu Al-Majd <khaledabualmajd06@gmail.com>
2026-02-22 09:43:54 +0200

    feat(FinCategory-Shared.DTOs): Create FinCategoryDTO, FinCategoryPagedFilterDTO, FinCategorySelectPagedFilterDTO and BudgetCheckDTO #1

---
+++ MoneyMindManager.Shared.DTOs/IncomeAndExpenseCategory/FinCategorySelectPagedFilterDTO.cs
@@ -0,0 +1,24 @@
+﻿using MoneyMindManager.Core.Enums;
+
+namespace MoneyMindManager.Shared.DTOs.IncomeAndExpenseCategory
+{
+    public class FinCategorySelectPagedFilterDTO
+    {
+        public string CategoryName { get; set; }
+        public bool? IsIncome { get; set; }
+        public enTextSearchMode TextSearchMode { get; set; }
+        public int PageNumber { get; set; }
+
+        public FinCategorySelectPagedFilterDTO(enTextSearchMode textSearchMode)
+        {
+            this.TextSearchMode = textSearchMode;
+            this.PageNumber = 1;
+        }
+
+        public FinCategorySelectPagedFilterDTO()
+        {
+            this.TextSearchMode = enTextSearchMode.WordsPrefix_Fast;
+            this.PageNumber = 1;
+        }
+    }
+}
