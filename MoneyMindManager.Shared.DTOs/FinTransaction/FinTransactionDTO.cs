7ff6569fde578b83e342d8be50838261b3fa2bf8
Khaled Abu Al-Majd <khaledabualmajd06@gmail.com>
2026-02-22 19:02:37 +0200

    feat(FinTransaction-Shared.DTOs):Create FinTransactionDTO #1

---
+++ MoneyMindManager.Shared.DTOs/FinTransaction/FinTransactionDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using MoneyMindManager.Shared.DTOs.MainTransaction;
+
+namespace MoneyMindManager.Shared.DTOs.FinTransaction
+{
+    public class FinTransactionDTO : MainTransactionDTO
+    {
+        public int? VoucherID { get; set; }
+        public int? CategoryID { get; set; }
+        public FinTransactionDTO(int? transactionID, decimal amount, DateTime createdDate, short? accountID, int? createdByUserID, byte? tranasactionTypeID, string purpose,
+            bool isLocked, DateTime transactionDate, string transactionTypeName, string createdByUserName, int? voucherID, int? categoryID)
+            : base(transactionID, amount, createdDate, accountID, createdByUserID, tranasactionTypeID, purpose, isLocked, transactionDate, transactionTypeName, createdByUserName)
+        {
+            this.VoucherID = voucherID;
+            this.CategoryID = categoryID;
+        }
+    }
+}
