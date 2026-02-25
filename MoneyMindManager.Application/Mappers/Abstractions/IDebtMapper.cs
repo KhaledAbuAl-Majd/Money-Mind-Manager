6561bd6b74af479af1f0e75460898c74a9f3d13d
Khaled Abu Al-Majd <khaledabualmajd06@gmail.com>
2026-02-25 15:14:55 +0200

    feat(Debt-Application-Mapper): Create IDebtMapper and DebtMapper #1

---
+++ MoneyMindManager.Application/Mappers/Abstractions/IDebtMapper.cs
@@ -0,0 +1,13 @@
+﻿using MoneyMindManager.Application.Abstractions.Mappers;
+using MoneyMindManager.Domain.Criteria.Debt;
+using MoneyMindManager.Domain.Entities;
+using MoneyMindManager.Shared.DTOs.Debt;
+
+namespace MoneyMindManager.Application.Mappers.Abstractions
+{
+    public interface IDebtMapper : IMapper<Debt, DebtDTO>
+    {
+        DebtSearchCriteria ToSearchCriteria(DebtFilterDTO DTO);
+        DebtPagedSearchCriteria ToPagedSearchCriteria(DebtPagedFilterDTO DTO);
+    }
+}
