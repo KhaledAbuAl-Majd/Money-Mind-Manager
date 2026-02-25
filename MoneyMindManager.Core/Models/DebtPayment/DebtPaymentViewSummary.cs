8a4d11e4aa8b6ac4557f9c82b93ec32e4b237095
Khaled Abu Al-Majd <khaledabualmajd06@gmail.com>
2026-02-25 18:53:23 +0200

    feat(DebtPayment-Core):Create DebtPaymentExportSummary and DebtPaymentViewSummary #1

---
+++ MoneyMindManager.Core/Models/DebtPayment/DebtPaymentViewSummary.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MoneyMindManager.Core.Models.DebtPayment
+{
+    public class DebtPaymentViewSummary
+    {
+        public int? MainTransactionID { get; set; }
+        public Decimal Amount { get; set; }
+        public DateTime DebtDate { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string CreatedByUserName { get; set; }
+        public string Purpose { get; set; }
+
+        public DebtPaymentViewSummary(int transactionID, decimal amount, DateTime debtDate, string createdByUserName,
+            DateTime createdDate, string purpose)
+        {
+            this.MainTransactionID = transactionID;
+            this.Amount = amount;
+            this.DebtDate = debtDate;
+            this.CreatedDate = createdDate;
+            this.CreatedByUserName = createdByUserName;
+            this.Purpose = purpose;
+        }
+    }
+}
