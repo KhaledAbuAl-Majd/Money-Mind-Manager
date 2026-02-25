197e6a935073e196ac611bc8c6990ff429ab0914
Khaled Abu Al-Majd <khaledabualmajd06@gmail.com>
2026-02-25 18:56:58 +0200

    feat(DebtPayment-Application-Mapper): Create IDebtPaymentMapper and DebtPaymentMapper #1

---
+++ MoneyMindManager.Application/Mappers/Abstractions/IDebtPaymentMapper.cs
@@ -0,0 +1,10 @@
+﻿using MoneyMindManager.Application.Abstractions.Mappers;
+using MoneyMindManager.Domain.Entities.DebtPayment;
+using MoneyMindManager.Shared.DTOs.DebtPayment;
+
+namespace MoneyMindManager.Application.Mappers.Abstractions
+{
+    public interface IDebtPaymentMapper : IMapper<DebtPayment, DebtPaymentDTO>
+    {
+    }
+}
