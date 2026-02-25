3496ce845e8b2c4579d17e911f164a3bf9063840
Khaled Abu Al-Majd <khaledabualmajd06@gmail.com>
2026-02-25 15:12:40 +0200

    feat(Debt-Shared.DTOs): Create DebtDTO, DebtFilterDTO, DebtPagedFilterDTO and DebtUpdateResultDTO #1

---
+++ MoneyMindManager.Shared.DTOs/Debt/DebtDTO.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using MoneyMindManager.Shared.DTOs.MainTransaction;
+
+namespace MoneyMindManager.Shared.DTOs.Debt
+{
+    public class DebtDTO : MainTransactionDTO
+    {
+        public int? DebtID { get; set; }
+        public bool IsLending { get; set; }
+        public int? PersonID { get; set; }
+        public DateTime? PaymentDueDate { get; set; }
+        public decimal RemainingAmount { get; set; }
+
+        //composition
+        public PersonDTO PersonInfo { get; set; }
+
+        public DebtDTO(int? transactionID, decimal amount, DateTime createdDate, short? accountID, int? createdByUserID, byte? tranasactionTypeID, string purpose,
+             bool isLocked, DateTime transactionDate, string transactionTypeName, string createdByUserName,
+             int? debtID, bool isLending, int? personID, DateTime? paymentDueDate, decimal remaintAmount)
+             : base(transactionID, amount, createdDate, accountID, createdByUserID, tranasactionTypeID, purpose, isLocked, transactionDate, transactionTypeName, createdByUserName)
+        {
+            this.DebtID = debtID;
+            this.IsLending = isLending;
+            this.PersonID = personID;
+            this.PaymentDueDate = paymentDueDate;
+            this.RemainingAmount = remaintAmount;
+        }
+    }
+}
---
+++ MoneyMindManager.Shared.DTOs/Debt/DebtPagedFilterDTO.cs
@@ -0,0 +1,20 @@
+﻿using MoneyMindManager.Core.Enums;
+
+namespace MoneyMindManager.Shared.DTOs.Debt
+{
+    public class DebtPagedFilterDTO : DebtFilterDTO
+    {
+        public int PageNumber { get; set; }
+
+        public DebtPagedFilterDTO(enTextSearchMode textSearchMode, int pageNumber) : base(textSearchMode)
+        {
+            this.PageNumber = 1;
+        }
+
+        public DebtPagedFilterDTO()
+        {
+            this.TextSearchMode = enTextSearchMode.WordsPrefix_Fast;
+            this.PageNumber = 1;
+        }
+    }
+}
