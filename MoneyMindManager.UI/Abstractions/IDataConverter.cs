86249e80ca6d8aca5d1c7505786a530527194536
Khaled Abu Al-Majd <khaledabualmajd06@gmail.com>
2026-02-27 20:55:06 +0200

    feat(DataConverter-UI): Create IDataConverter, DataConverterService #1

---
+++ MoneyMindManager.UI/Abstractions/IDataConverter.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Data;
+
+namespace MoneyMindManager.UI.Abstractions
+{
+    public interface IDataConverter
+    {
+        DataTable ToDataTable<T>(IEnumerable<T> data);
+    }
+}
---
+++ MoneyMindManager.UI/Services/DataConverterService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using MoneyMindManager.UI.Abstractions;
+
+namespace MoneyMindManager.UI.Services
+{
+    public class DataConverterService : IDataConverter
+    {
+        public DataTable ToDataTable<T>(IEnumerable<T> data)
+        {
+            if (data is null)
+                return null;
+
+            DataTable dt = new DataTable();
+
+            var proprties = typeof(T).GetProperties();
+
+            foreach (var property in proprties)
+            {
+                dt.Columns.Add(property.Name, property.PropertyType);
+            }
+
+            foreach (var item in data)
+            {
+                var dr = dt.NewRow();
+                foreach (var propery in proprties)
+                {
+                    dr[propery.Name] = propery.GetValue(item);
+                }
+                dt.Rows.Add(dr);
+            }
+
+            return dt;
+        }
+    }
+}
