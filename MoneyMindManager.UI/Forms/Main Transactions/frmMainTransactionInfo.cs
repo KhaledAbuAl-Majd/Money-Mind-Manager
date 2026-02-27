88170043dc0c028f03a188a1d434b174493d453d
Khaled Abu Al-Majd <khaledabualmajd06@gmail.com>
2026-02-27 20:58:28 +0200

    refactoring(MainTransaction-UI-Forms): frmMainTransactionInfo #1

--- MoneyMindManager.UI/Forms/Main Transactions/frmMainTransactionInfo.cs
+++ MoneyMindManager.UI/Forms/Main Transactions/frmMainTransactionInfo.cs
@@ -1,27 +1,47 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using MoneyMindManager.Client.Abstractions.ApiClient;
+using MoneyMindManager.UI.Abstractions;
 
 namespace MoneyMindManager_Presentation.Transactions
 {
     public partial class frmMainTransactionInfo : Form
     {
-        public frmMainTransactionInfo(int transactionID)
+        private IUserSession _userSession;
+        private IMessageBoxService _messageBoxService;
+        private IMainTransactionApiClient _mainTransactionApiClient;
+        private IFormDisplayer _formDisplayer;
+        private bool isInitialized = false;
+        public frmMainTransactionInfo(IUserSession userSession, IMessageBoxService messageBoxService, IMainTransactionApiClient mainTransactionApiClient
+          , IFormDisplayer formDisplayer)
         {
             InitializeComponent();
+            this._userSession = userSession;
+            this._messageBoxService = messageBoxService;
+            this._mainTransactionApiClient = mainTransactionApiClient;
+            this._formDisplayer = formDisplayer;
+        }
+
+        public bool Initilize(int transactionID)
+        {
+            if (!ctrlMainTransactionInfo1.Initialize(_userSession, _messageBoxService, _mainTransactionApiClient, _formDisplayer))
+                return false;
+
+            this.isInitialized = true;
             this._transactionID = transactionID;
+            return true;
         }
 
         int _transactionID;
 
         private async void frmMainTransactionInfo_Load(object sender, EventArgs e)
         {
+            if (!isInitialized)
+            {
+                this.Close();
+                return;
+            }
+
             if (!await this.ctrlMainTransactionInfo1.LoadMainTransaction(_transactionID))
                 this.Close();
         }
