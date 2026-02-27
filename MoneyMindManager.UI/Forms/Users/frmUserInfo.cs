946cabd9fcce8ca2759209f72712a75475caccc5
Khaled Abu Al-Majd <khaledabualmajd06@gmail.com>
2026-02-27 14:46:03 +0200

    refactoring(User-Ui-Forms): frmUserInfo #1

--- MoneyMindManager.UI/Forms/Users/frmUserInfo.cs
+++ MoneyMindManager.UI/Forms/Users/frmUserInfo.cs
@@ -1,41 +1,52 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using MoneyMindManager.Client.Abstractions.ApiClient;
+using MoneyMindManager.UI.Abstractions;
+using MoneyMindManager_Presentation.People.Controls;
 
 namespace MoneyMindManager_Presentation.Users
 {
     public partial class frmUserInfo : Form
     {
-        
+        private IPersonApiClient _personApiClient;
+        private IUserSession _userSession;
+        private IMessageBoxService _messageBoxService;
+        private IUserApiClient _userApiClient;
+        private IFormDisplayer _formDisplayer;
+
+        private bool isInitialized = false;
+
         public event Action OnEditingUserAndFormClosed;
 
         bool _IsUserEdited = false;
 
         int _userID;
-        public frmUserInfo(int userID)
+
+        public frmUserInfo(IPersonApiClient personApiClient, IUserSession userSession, IMessageBoxService messageBoxService,
+           IUserApiClient userApiClient, IFormDisplayer formDisplayer)
         {
             InitializeComponent();
-            //this._userID = userID;
-        }
-        public frmUserInfo()
-        {
-            InitializeComponent();
-            //this._userID = userID;
+            this._personApiClient = personApiClient;
+            this._userSession = userSession;
+            this._messageBoxService = messageBoxService;
+            this._userApiClient = userApiClient;
+            this._formDisplayer = formDisplayer;
         }
 
-        public void Initialize(int userID)
+        public bool Initialize(int userID)
         {
+            if (!ctrlUserCard1.Initialize(_personApiClient, _userSession, _messageBoxService, _userApiClient, _formDisplayer))
+                return false;
             this._userID = userID;
+            this.isInitialized = true;
+            return true;
         }
 
         private async void frmUseInfo_Load(object sender, EventArgs e)
         {
+            if (!isInitialized)
+                this.Close();
+
             if (!await ctrlUserCard1.LoadUser(_userID))
                 this.Close();
 
