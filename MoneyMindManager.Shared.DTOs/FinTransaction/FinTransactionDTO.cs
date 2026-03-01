0a75ce52acff2f16a8fe9f34459ac3c90b7f4271
Khaled Abu Al-Majd <khaledabualmajd06@gmail.com>
2026-03-01 15:19:31 +0200

    refactoring(FinTransaction-Shared.DTOs): add empty constructuror and CategoryInfo Property-CompositionCategoryDTO #1

--- MoneyMindManager.Shared.DTOs/FinTransaction/FinTransactionDTO.cs
+++ MoneyMindManager.Shared.DTOs/FinTransaction/FinTransactionDTO.cs
@@ -7,12 +7,19 @@ namespace MoneyMindManager.Shared.DTOs.FinTransaction
     {
         public int? VoucherID { get; set; }
         public int? CategoryID { get; set; }
+
+        public FinCategoryDTO CategoryInfo { get; set; }
         public FinTransactionDTO(int? transactionID, decimal amount, DateTime createdDate, short? accountID, int? createdByUserID, byte? tranasactionTypeID, string purpose,
             bool isLocked, DateTime transactionDate, string transactionTypeName, string createdByUserName, int? voucherID, int? categoryID)
             : base(transactionID, amount, createdDate, accountID, createdByUserID, tranasactionTypeID, purpose, isLocked, transactionDate, transactionTypeName, createdByUserName)
         {
             this.VoucherID = voucherID;
             this.CategoryID = categoryID;
+        }
+
+        public FinTransactionDTO() : base(null, default, default, default, null, null, null, default, default, default, default)
+        {
+
         }
     }
 }
