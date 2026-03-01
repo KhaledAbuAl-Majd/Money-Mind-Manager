059ed419841819c9bf455062f7dd514520e5dfcd
Khaled Abu Al-Majd <khaledabualmajd06@gmail.com>
2026-03-01 15:22:53 +0200

    refactoring(FinTransaction-UI-Forms): frmFinTransactionInfo #1

--- MoneyMindManager.UI/Forms/Financial-Income And Expense/Financial-IAE_Transactions/frmFinTransactionInfo.cs
+++ MoneyMindManager.UI/Forms/Financial-Income And Expense/Financial-IAE_Transactions/frmFinTransactionInfo.cs
@@ -1,36 +1,65 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using MoneyMindManager_Business;
+using MoneyMindManager.Client.Abstractions.ApiClient;
+using MoneyMindManager.Shared.DTOs.FinTransaction;
+using MoneyMindManager.Shared.DTOs.MainTransaction;
+using MoneyMindManager.UI.Abstractions;
 
 namespace MoneyMindManager_Presentation.Transactions
 {
     public partial class frmFinTransactionInfo : Form
     {
-        public frmFinTransactionInfo(int transactionID)
+        private IUserSession _userSession;
+        private IMessageBoxService _messageBoxService;
+        private IMainTransactionApiClient _mainTransactionApiClient;
+        private IFormDisplayer _formDisplayer;
+        private IFinTransactionApiClient _finTransactionApi;
+        private bool isInitialized = false;
+        public frmFinTransactionInfo(IUserSession userSession, IMessageBoxService messageBoxService, IMainTransactionApiClient mainTransactionApiClient
+          , IFormDisplayer formDisplayer, IFinTransactionApiClient finTransactionApiClient)
         {
             InitializeComponent();
+            this._userSession = userSession;
+            this._messageBoxService = messageBoxService;
+            this._mainTransactionApiClient = mainTransactionApiClient;
+            this._formDisplayer = formDisplayer;
+            this._finTransactionApi = finTransactionApiClient;
+        }
+
+        public bool Initilize(int transactionID)
+        {
+            if (!ctrlMainTransactionInfo1.Initialize(_userSession, _messageBoxService, _mainTransactionApiClient, _formDisplayer))
+                return false;
+
+            this.isInitialized = true;
             this._transactionID = transactionID;
+            return true;
         }
 
         int _transactionID;
 
-        clsIncomeAndExpenseTransaction _IAETransactionInfo;
+        FinTransactionDTO _IAETransactionInfo;
 
         private async void frmMainTransactionInfo_Load(object sender, EventArgs e)
         {
-            _IAETransactionInfo = await clsIncomeAndExpenseTransaction.FindTransactionByTransactionID(_transactionID);
-
-            if (_IAETransactionInfo == null)
+            if (!isInitialized)
+            {
                 this.Close();
+                return;
+            }
 
-            if (!ctrlMainTransactionInfo1.LoadMainTransaction((clsMainTransaction)_IAETransactionInfo))
+            var result = await _finTransactionApi.Get(_transactionID, Convert.ToInt32(_userSession.UserID));
+
+            if (!result.IsSuccess || result.Data is null)
+            {
+                _messageBoxService.DisplayError(result.ErrorMessage);
+                this.Close();
+                return;
+            }
+
+            _IAETransactionInfo = result.Data;
+
+            if (!ctrlMainTransactionInfo1.LoadMainTransaction((MainTransactionDTO)_IAETransactionInfo))
                 this.Close();
 
             this.kgtxtCategoryName.Text = _IAETransactionInfo.CategoryInfo?.CategoryName;
