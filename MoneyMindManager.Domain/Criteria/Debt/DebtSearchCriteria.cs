8064bc4dd986e48ff82f71a7544d9027fbb1d171
Khaled Abu Al-Majd <khaledabualmajd06@gmail.com>
2026-03-02 08:02:30 +0200

    refactoring(Debt-Domain-SearchCriteria): make IsLending and IsPaid NULLABLE #1

--- MoneyMindManager.Domain/Criteria/Debt/DebtSearchCriteria.cs
+++ MoneyMindManager.Domain/Criteria/Debt/DebtSearchCriteria.cs
@@ -6,14 +6,14 @@ namespace MoneyMindManager.Domain.Criteria.Debt
     public class DebtSearchCriteria
     {
         public int? DebtID { get; set; }
-        public bool IsLending { get; set; }
+        public bool? IsLending { get; set; }
         public string PersonName { get; set; }
         public string UserName { get; set; }
         public DateTime? FromCreatedDate { get; set; }
         public DateTime? ToCreatedDate { get; set; }
         public DateTime? FromDebtDate { get; set; }
         public DateTime? ToDebtDate { get; set; }
-        public bool IsPaid { get; set; }
+        public bool? IsPaid { get; set; }
         public byte TextSearchMode { get; set; }
 
         public DebtSearchCriteria(enTextSearchMode textSearchMode)
