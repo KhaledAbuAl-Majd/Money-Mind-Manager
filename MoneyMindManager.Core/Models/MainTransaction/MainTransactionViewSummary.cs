7d823ce12510d93467d67616f77685ae0b13e7ef
Khaled Abu Al-Majd <khaledabualmajd06@gmail.com>
2026-03-04 17:23:30 +0200

    feat(MainTransaction-Core): Create MainTransactionViewSummary and MainTransactionExportSummary #1

---
+++ MoneyMindManager.Core/Models/MainTransaction/MainTransactionViewSummary.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MoneyMindManager.Core.Models.MainTransaction
+{
+    public class MainTransactionViewSummary
+    {
+        public int? MainTransactionID { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime TransactionDate { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string CreatedByUserName { get; set; }
+        public string Purpose { get; set; }
+        public string TransactionTypeName { get; set; }
+
+        public MainTransactionViewSummary(int? transactionID, decimal amount, DateTime createdDate,
+            string purpose, DateTime transactionDate, string createdByUserName, string transactionTypeName)
+        {
+            this.MainTransactionID = transactionID;
+            this.Amount = amount;
+            this.CreatedDate = createdDate;
+            this.Purpose = purpose;
+            this.TransactionDate = transactionDate;
+            this.CreatedByUserName = createdByUserName;
+            this.TransactionTypeName = transactionTypeName;
+        }
+    }
+}
