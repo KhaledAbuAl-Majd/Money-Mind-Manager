67a26bfd1d0b360bbe4e7f6c4aab2e79ea805f55
Khaled Abu Al-Majd <khaledabualmajd06@gmail.com>
2026-03-05 17:16:32 +0200

    feat(Person-Core): Create PersonFullSummary and PersonSelectSummary #1

---
+++ MoneyMindManager.Core/Models/Person/PersonFullSummary.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MoneyMindManager.Core.Models.Person
+{
+    public class PersonFullSummary
+    {
+        public Nullable<int> PersonID { get; set; }
+        public string PersonName { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+
+        public PersonFullSummary(int? personID, string personName, string address, string email, string phone)
+        {
+            this.PersonID = personID;
+            this.PersonName = personName;
+            this.Address = address;
+            this.Email = email;
+            this.Phone = phone;
+        }
+    }
+}
